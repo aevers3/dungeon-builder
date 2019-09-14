75859e140c87c507a29d1a295129f21493b9d39c
Alex Evers <aevers@sealworks.com>
2019-09-14 08:16:36 -0400

    initial files

---
+++ client/src/components/Posts.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="posts">
+    This file will list all the posts.
+  </div>
+</template>
+
+<script>
+console.log('posts')
+export default {
+  name: 'Posts',
+  data () {
+    return {
+      msg: 'hii'
+    }
+  }
+}
+</script>
